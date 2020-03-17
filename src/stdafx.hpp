4591f2af7ceaef4b12d00a7f622d524a7eb4cc10
leo <coderleo42+leocodes@gmail.com>
2020-03-17 18:53:51 -0400

    [main] change .h files to .hpp

--- src/stdafx.hpp
+++ src/stdafx.hpp
@@ -2,12 +2,12 @@
 // Created by coder on 3/17/2020.
 //
 
-#ifndef MODLOADER_STDAFX_H
-#define MODLOADER_STDAFX_H
+#ifndef MODLOADER_STDAFX_HPP
+#define MODLOADER_STDAFX_HPP
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <cstdio>
 #include <string>
 
-#endif //MODLOADER_STDAFX_H
+#endif //MODLOADER_STDAFX_HPP
