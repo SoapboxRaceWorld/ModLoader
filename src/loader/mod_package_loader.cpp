4639d8c8c36de8dbf12c587e6f0d9e12338aaf0e
leo <coderleo42+leocodes@gmail.com>
2020-03-18 10:17:23 -0400

    [loader] begin implementing 7z support; get encryption key from file

--- src/loader/mod_package_loader.cpp
+++ src/loader/mod_package_loader.cpp
@@ -6,8 +6,12 @@
 #include "mod_package_header.hpp"
 #include <fstream>
 #include <utility>
-#include <fmt/format.h>
-#include "../stdafx.hpp"
+#include <bitextractor.hpp>
+#include <bitexception.hpp>
+
+static const unsigned char key_xor_table[] = {
+        0x94, 0xce, 0xc3, 0xae, 0x73, 0xf9, 0xf1, 0xb9
+};
 
 mod_package_loader::mod_package_loader(fs::path path) : m_path_(std::move(path)) {
 
@@ -39,6 +43,32 @@ std::shared_ptr<mod_package> mod_package_loader::load() {
     if (archive_size == 0) {
         return nullptr;
     }
+
+    std::string archive_key;
+
+    if (header.encryption_enabled) {
+        unsigned int key_length = 0;
+
+        stream.read((char *) &key_length, sizeof(key_length));
+
+        if (key_length != 32) {
+            return nullptr;
+        }
+
+        unsigned char key[33] = {0};
+
+        stream.read((char *) key, 32);
+
+        for (int i = 0; i < 32; i++) {
+            key[i] ^= key_xor_table[i % sizeof(key_xor_table)];
+        }
+
+        archive_key = std::string(key, key + 32);
+    }
+
+    // Set up 7z interface
+    bit7z::Bit7zLibrary lib(L"7z.dll");
+    bit7z::BitExtractor extractor(lib, bit7z::BitFormat::Zip);
 
 
     return nullptr;
