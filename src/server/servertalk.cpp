d0638e6cc42ccb061686705007c06e9adbd10c35
leo <coderleo42+leocodes@gmail.com>
2020-07-07 12:05:46 -0400

    change from cpr to httplib

--- src/server/servertalk.cpp
+++ src/server/servertalk.cpp
@@ -1,11 +1,15 @@
 //
 // Created by coder on 3/17/2020.
 //
-
 #include "servertalk.hpp"
 #include "servertalkexception.hpp"
 
-#include <cpr/cpr.h>
+#define CPPHTTPLIB_ZLIB_SUPPORT
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+#include <wincrypt.h>
+#include "../httplib.hpp"
+//#include <cpr/cpr.h>
 #include <fmt/format.h>
 #include <utility>
 
@@ -13,18 +17,16 @@ server_talk::server_talk(std::string server_address) : m_server_address_(std::mo
 }
 
 std::shared_ptr<server::modding_info> server_talk::get_modding_info() {
-    auto r = cpr::Get(cpr::Url{m_server_address_ + "/Modding/GetModInfo"});
+    auto res = httplib::Client2(m_server_address_.c_str()).set_decompress(true).Get("/Modding/GetModInfo");
+    auto body = res->body;
 
-    if (r.error) {
-        throw server_talk_exception(fmt::format("GetModInfo() failed with code {0}", r.error.code));
-    }
-
-    if (r.status_code != 200 || r.text.empty()) {
+    if (res->status != 200 || body.empty()) {
         return nullptr;
     }
 
+    MessageBoxA(nullptr, body.c_str(), "debug", MB_OK);
 
-    nlohmann::json j = nlohmann::json::parse(r.text);
+    nlohmann::json j = nlohmann::json::parse(body);
 
     return std::make_shared<server::modding_info>(j.get<server::modding_info>());
 }
